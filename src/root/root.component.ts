45911af15dbfeb3a1683436b5ec17992eb53e9a6
Akanksha Sharma <akanksha_sh@HCL.COM>
2019-09-12 17:14:42 +0530

    UI for project page

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 
 @Component({
@@ -7,9 +8,25 @@ import { Component } from '@angular/core';
 })
 export class RootComponent {
 
+  public isScrolled = false;
+
   //Go to top
   goToTop() {
     window.scrollTo(0,0);
   }
+
+constructor(@Inject(DOCUMENT) private document: any) {}
+@HostListener('window:scroll', [])
+onWindowScroll() {
+    const number = window.scrollY;
+    var divToChange = document.getElementById('goToButton');
+    if (number > 200) {
+        this.isScrolled = true;
+        divToChange.classList.add('active'); 
+    } else if (this.isScrolled && number < 10) {
+        this.isScrolled = false;
+        divToChange.classList.remove('active'); 
+    }
+}
 }
 
